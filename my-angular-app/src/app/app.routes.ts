643e2e69be24048b1a3d546c130389af52c46b12
Hardeep <bhoocorp@gmail.com>
2026-01-19 21:03:24 -0500

    initial commit

--- my-angular-app/src/app/app.routes.ts
+++ my-angular-app/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { HeaderComponent } from './header/header.component';  
 import { FooterComponent } from "./footer/footer.component";  
 import { AboutComponent } from "./about/about.component";
-import { BioComponent } from "./bio/bio.component";
+import { BioComponent } from "./bio/bio.comopnent";
 import { SkillsComponent } from './skills/skills.component';
 import { ContactUsComponent } from './contact-us/contact-us.component';
 
--- my-angular-app/src/app/main-page/main-page.component.ts
+++ my-angular-app/src/app/main-page/main-page.component.ts
@@ -2,12 +2,18 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from "../footer/footer.component";
+import { AboutComponent } from "../about/about.component";
+import { BioComponent } from "../bio/bio.comopnent";
+import { SkillsComponent } from '../skills/skills.component';
+import { ExperienceComponent } from '../experience/experience.component'; 
+import { ContactUsComponent } from '../contact-us/contact-us.component';    
+
 
 
 @Component({
   selector: "app-main-page",
   standalone: true,
-  imports: [CommonModule, HeaderComponent, FooterComponent],
+  imports: [CommonModule, HeaderComponent, AboutComponent, BioComponent,SkillsComponent,ExperienceComponent, FooterComponent],
   templateUrl: "./main-page.html",
   //styleUrl: "./main-page.component.css", 
 
