9e2d6104641a1d47c2d73695cad047fa4d3e6086
bkdInc <bhoocorp@outlook.com>
2026-01-20 22:47:54 -0800

    Pages changed and Data file for Skills added

--- my-angular-app/src/app/skills/skills.component.ts
+++ my-angular-app/src/app/skills/skills.component.ts
@@ -1,4 +1,12 @@
 import { Component } from "@angular/core";
+import SkillsDataComponent from '../../assets/data/skills.json';
+
+// Define an interface for the data structure
+interface SkillsData {
+  id: number;
+  name: string;
+  value: number;
+}
 
 @Component({
   selector: "app-skills",
@@ -7,10 +15,13 @@ import { Component } from "@angular/core";
 export class SkillsComponent {
   title: string;
   content: string;
+  public items: SkillsData[] = SkillsDataComponent;
 
   constructor() {
     this.title = "Skills";
     this.content =
       "This is the About Us page where you can learn more about our mission and values.";
+    console.log(this.items);
   }
 }
+
