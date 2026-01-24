903c19735ce61a5ad92e7bcd8898aa2955ca17f4
bkdInc <bhoocorp@outlook.com>
2026-01-24 00:01:46 -0800

    Many changes in data files experience and html

--- my-angular-app/src/app/experience/experience.component.ts
+++ my-angular-app/src/app/experience/experience.component.ts
@@ -1,5 +1,18 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import ExperienceDataComponent from '../../assets/data/experience.json';
+
+// Define an interface for the data structure
+interface ExperienceData {
+  id: number;
+  dateFrom: string;
+  dateTo: string;
+  position: string;
+  company: string;
+  location: string;
+  description: string;
+}
+
 
 @Component({
   selector: "app-experience",
@@ -9,10 +22,12 @@ import { CommonModule } from "@angular/common";
 export class ExperienceComponent {
   title: string;
   content: string;
+   public items: ExperienceData[] = ExperienceDataComponent;
 
   constructor() {
     this.title = "Experience";
     this.content =
       "This is the About Us page where you can learn more about our mission and values.";
+      console.log(this.items);
   }
 }
