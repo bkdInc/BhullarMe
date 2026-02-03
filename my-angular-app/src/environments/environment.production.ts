a4a6c85c846319daf5a78875432f8795d15fdea3
bkdinc <bhoocorp@outlook.com>
2026-02-03 15:30:15 -0500

    Update Angular to use Render backend URL

--- my-angular-app/src/environments/environment.production.ts
+++ my-angular-app/src/environments/environment.production.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   cvUrl: '/assets/HB_Director_Resume_10_Years_Experience.pdf',
-  EMAIL_API_URL: 'https://bhullar.me/api/send-email'
+  EMAIL_API_URL: 'https://bhullar-backend.onrender.com/api/send-email'
 };
